0963f998513913db9d72747884c7725eb680384c
Normanbitu <157604384+Normanbitu@users.noreply.github.com>
2024-01-30 10:12:19 -0800

    Add files via upload

---
+++ code.py
@@ -0,0 +1,5 @@
+import datetime
+file=open("output.txt","w+")
+
+file.write("LSLAMIC UNIVERSITY IN UGANDA KAMPALA CAMPUS \n,\tNAME:\tBatukirewa Norman\n,\tREGNO:\t223-063011-25291\n\tlecturer: adbulhakim\n,\tdate:\t24/01/2024\n,\tcourseunit\t8\n,\t\t\t\t***END***")
+
